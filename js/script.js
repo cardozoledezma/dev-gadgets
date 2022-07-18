2e2e9de64e0a0cd0c0a5a92910f4a76ae75d4413
Guillaume Afpa <guillaume.belleuvre@afpa.fr>
2022-07-18 09:58:38 +0200

    get pictures list from HTML

--- js/script.js
+++ js/script.js
@@ -4,4 +4,14 @@ const mainPicture = document.getElementById("picture-main");
 const buttonPrev = document.getElementById("picture-prev");
 const buttonNext = document.getElementById("picture-next");
 
-console.log(mainPicture, buttonPrev, buttonNext);
+function getPicturesLarge() {
+    const list = [];
+    document.querySelectorAll("#thumbs img[data-img-large]").forEach(img => {
+        list.push(img.dataset.imgLarge);
+    })
+    return list;
+}
+
+const picturesList = getPicturesLarge();
+
+console.log(picturesList);
