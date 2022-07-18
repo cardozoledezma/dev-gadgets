5e778c965c43a0048bc3ff7ba969a4d9ab90ebf3
Guillaume Afpa <guillaume.belleuvre@afpa.fr>
2022-07-18 10:29:34 +0200

    manage gallery on desktop

--- js/script.js
+++ js/script.js
@@ -4,9 +4,13 @@ let imgCount = 0;
 const picturesList = getPicturesLarge();
 manageGalleryHandlers();
 
+function getThumbsElements() {
+    return document.querySelectorAll("#thumbs img[data-img-large]");
+}
+
 function getPicturesLarge() {
     const list = [];
-    document.querySelectorAll("#thumbs img[data-img-large]").forEach(img => {
+    getThumbsElements().forEach(img => {
         list.push(img.dataset.imgLarge);
     })
     return list;
@@ -19,18 +23,23 @@ function manageGalleryHandlers() {
             imgCount = picturesList.length-1;
         }
         displayPicture(picturesList[imgCount]);
-    })
+    });
+
     document.getElementById("picture-next").addEventListener("click", function(event) {
         imgCount++;
         if (imgCount > picturesList.length-1) {
             imgCount = 0;
         }
         displayPicture(picturesList[imgCount]);
-    })
+    });
+
+    getThumbsElements().forEach(thumb => {
+        thumb.addEventListener("mouseover", function(event) {
+            displayPicture(event.target.dataset.imgLarge);
+        })
+    });
 }
 
 function displayPicture(imgSrc) {
     document.getElementById("picture-main").src = imgSrc;
 }
-
-
